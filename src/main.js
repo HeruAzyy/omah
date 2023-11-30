3679296cc10fb2d7929252f7a6d5003551ae8fbb
Heru Azy <heruazyp@gmail.com>
2023-11-30 23:36:55 +0700

    Add: Fixed nav saat scroll

--- src/main.js
+++ src/main.js
@@ -1,3 +1,14 @@
+window.onscroll = function () {
+  const navbar = document.getElementById("navbar");
+  const fixedNav = navbar.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+};
+
 const btnSideBar = document.getElementById("btn-side-bar");
 const sideBar = document.getElementById("side-bar");
 
